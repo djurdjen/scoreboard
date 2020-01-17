421b6b727f7aa681d754ed921fed4fd152d4d107
Jurian Koning <jurian@passionatepeople.io>
2020-01-17 15:29:05 +0100

    fix for dev tools

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ var bodyParser = require("body-parser");
 var app = express();
 const root = path.join(__dirname, "build");
 
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    !process.env.DEV
+  ) {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
+
 // view engine setup
 app.use(logger("dev"));
 app.use(bodyParser.json());
--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,20 @@ import rootReducer from "./reducers";
 import middleware from "./middleware";
 
 const savedState = JSON.parse(localStorage.getItem("currentGame"));
+let middlwareComposition = null;
+if (process.env.NODE_ENV === "development") {
+  middlwareComposition = compose(
+    applyMiddleware(...middleware),
+
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //init devtools extension
+  );
+} else {
+  middlwareComposition = compose(applyMiddleware(...middleware));
+}
 const store = createStore(
   rootReducer,
   savedState ? { game: JSON.parse(localStorage.getItem("currentGame")) } : {},
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //init devtools extension
-  )
+  middlwareComposition
 );
 
 export default store;
